5c3aa36b3d9a8efa99b707b4d4e171f80273845b
Karol Mąkowski <Karol Mkowski>
2016-06-28 00:30:51 +0200

    more improvements

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var question = prompt('Type Your Name, Plese');
 var welcomeText = document.getElementById('welcomeText');
 // Title
-if (question == null) {	
+if (question === null) {	
 	welcomeText.innerHTML = 'Hello My Dear Guest';
 }
 else {
@@ -10,11 +10,10 @@ else {
 }
 // Table
 document.getElementById("playerName").innerHTML = question;
-question = player.name
 
 // === Game ====
 var player = {
-	name: "",
+	name: question,
 	score: 0,
 	pick: ""
 };
@@ -33,11 +32,6 @@ computer.pick = "";
 player.score = 0;
 player.pick = "";
 round = 0;
-
-//Player pick
-function playerPick(pick) {
-	player.pick = pick; 
-}
 
 // Hidden buttons, visible after 'newGame' button click
 function newGame() {
@@ -62,35 +56,72 @@ function computerPick() {
 	}
 }
 
+var gameResult;
 // Check the results
 function checkResult() {
-	var gameResult;
 	if (player.pick === computer.pick) {	//Check if there is a tie
-	gameResult = "tie!";
+		gameResult = "tie!";
 	}
 	else if (player.pick === "rock") {		//if player chooses rock    
     	if (computer.pick === "scissors") {
-    		player.score++; gameResult = "Player wins!";
+    		player.score++;
+    		gameResult = player.name + " wins!";
     	} 
     	else if (computer.pick === "paper") {
-    		computer.score++; gameResult = "Computer wins!";
+    		computer.score++;
+    		gameResult = "Computer wins!";
     	}
 	}
 	else if (player.pick === "paper") {		//if player chooses paper
 		if  (computer.pick === "rock") {
-			player.score++; gameResult = "Player wins!";
+			player.score++;
+			gameResult = player.name + " wins!";
 		}
 		else if (computer.pick === "scissors") {
-			computer.score++; gameResult = "Computer wins!";
+			computer.score++;
+			gameResult = "Computer wins!";
 		}
 	}
 	else if (player.pick === "scissors") {	//if player chooses scissors
 		if  (computer.pick === "paper") {
-			player.score++; gameResult = "Player wins!";
+			player.score++;
+			gameResult = player.name + " wins!";
 		}
 		else if (computer.pick === "rock") {
-			computer.score++; gameResult = "Computer wins!";
+			computer.score++;
+			gameResult = "Computer wins!";
 		}
+	}
+
+document.getElementById("playerScore").innerHTML = player.score;
+document.getElementById("computerScore").innerHTML = computer.score;
+document.getElementById("computerPick").innerHTML = computer.pick;
+document.getElementById("playerPick").innerHTML = player.pick;
+document.getElementById("gameResult").innerHTML = gameResult;
+}
+
+// Current round player name
+document.getElementById("playerNameIs").innerHTML = player.name;
+
+function playerPick(pick) {
+	player.pick = pick; //save player's pick
+	computerPick(); //choose computer's pick
+	checkResult(); //check result  
+}
+
+// Score
+if ((player.score >= 10) || (computer.score >=10)) {
+	//log the result
+	if (player.score > computer.score) {
+		document.getElementById("roundResult").innerHTML = "player wins the game!";
+	}
+	else {
+		document.getElementById("roundResult").innerHTML = "computer wins the game!";
 	}
 }
 
+//hide buttons
+var buttons = document.getElementsByClassName("pick-button");
+for (var i = 0; i < buttons.length; i++) {
+	buttons[i].style.visibility = 'hidden';
+}
